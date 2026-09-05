f3719bf8ce1a3ff321a4467e71b262ac65b40091
michelle414 <55652743+michelle414@users.noreply.github.com>
2026-09-06 01:41:44 +0800

    Update run-command.js

--- scripts/lib/run-command.js
+++ scripts/lib/run-command.js
@@ -11,7 +11,7 @@ export function runCommand(command, args, options = {}) {
   return new Promise((resolve, reject) => {
     const mirrorOutput = options.stdio === 'inherit'
     const stdio = mirrorOutput ? ['inherit', 'pipe', 'pipe'] : options.stdio
-    const child = spawn(executableFor(command), finalArgs, { ...options, stdio, shell: process.platform === 'win32' })
+    const child = spawn(executableFor(command), finalArgs, { ...options, stdio, shell: process.platform === 'win32' && (executableFor(command).endsWith('.cmd') || executableFor(command).endsWith('.bat')) })
     let stdout = ''
     let stderr = ''
     child.stdout?.on('data', (chunk) => {
